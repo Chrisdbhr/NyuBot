7398bb0f7f93f13dc3697aaa6554abca466e07b2
Chris <christopher.borba@beetools.com.br>
2019-05-23 11:38:36 -0300

    Initial commit

---
+++ SourceCode/NyuBot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NyuBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            Console.WriteLine("Hello World!");
+            await Task.Delay(-1);
+        }
+    }
+}
