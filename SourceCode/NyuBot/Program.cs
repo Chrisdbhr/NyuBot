ba0270819195f94f3ef2501c8da4badb0e7c820b
Chris <christopher.borba@beetools.com.br>
2019-05-24 17:28:59 -0300

    first files

--- SourceCode/NyuBot/Program.cs
+++ SourceCode/NyuBot/Program.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using Discord;
+using System;
 using System.Threading.Tasks;
+using Discord.WebSocket;
 
 namespace NyuBot
 {
     public class Program
     {
+        private DiscordSocketClient _client;
+
+        
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _client.Log += Log;
+            
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+            await _client.StartAsync();
+
+            
             Console.WriteLine("Hello World!");
+
+            
+            // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
