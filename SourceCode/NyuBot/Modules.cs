8c191a95280c594bb20b92e8d2bb00a9d3152b72
Chris <christopher.borba@beetools.com.br>
2019-11-07 19:44:28 -0300

    template bot for commands add

--- SourceCode/NyuBot/Modules.cs
+++
@@ -1,7 +0,0 @@
-namespace NyuBot
-{
-    public class Modules
-    {
-        
-    }
-}
--- SourceCode/NyuBot/Program.cs
+++ SourceCode/NyuBot/Program.cs
@@ -1,48 +1,70 @@
-﻿using Discord;
-using System;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using _02_commands_framework.Services;
 
 namespace NyuBot
 {
-    public class Program
+    // This is a minimal example of using Discord.Net's command
+    // framework - by no means does it show everything the framework
+    // is capable of.
+    //
+    // You can find samples of using the command framework:
+    // - Here, under the 02_commands_framework sample
+    // - https://github.com/foxbot/DiscordBotBase - a bare-bones bot template
+    // - https://github.com/foxbot/patek - a more feature-filled bot, utilizing more aspects of the library
+    class Program
     {
-        private DiscordSocketClient _client;
-
-        
-        public static void Main(string[] args)
+        // There is no need to implement IDisposable like before as we are
+        // using dependency injection, which handles calling Dispose for us.
+        static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            _client = new DiscordSocketClient(new DiscordSocketConfig
+            // You should dispose a service provider created using ASP.NET
+            // when you are finished using it, at the end of your app's lifetime.
+            // If you use another dependency injection framework, you should inspect
+            // its documentation for the best way to do this.
+            using (var services = ConfigureServices())
             {
-                LogLevel = LogSeverity.Info
-            });
+                var client = services.GetRequiredService<DiscordSocketClient>();
 
-            _client.Log += Log;
-            
-            // Remember to keep token private or to read it from an 
-            // external source! In this case, we are reading the token 
-            // from an environment variable. If you do not know how to set-up
-            // environment variables, you may find more information on the 
-            // Internet or by using other methods such as reading from 
-            // a configuration.
-            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
-            await _client.StartAsync();
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
 
-            
-            Console.WriteLine("Hello World!");
+                // Tokens should be considered secret data and never hard-coded.
+                // We can read from the environment variable to avoid hardcoding.
+                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
+                await client.StartAsync();
 
-            
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
+                // Here we initialize the logic required to register our commands.
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
         }
 
-        private Task Log(LogMessage msg)
+        private Task LogAsync(LogMessage log)
         {
-            Console.WriteLine(msg.ToString());
+            Console.WriteLine(log.ToString());
+
             return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .BuildServiceProvider();
         }
     }
 }
