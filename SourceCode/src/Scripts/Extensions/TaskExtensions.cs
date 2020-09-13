8db0bcec81826864d3bc0ca27964a30d0bba5dd3
Chrisdbhr <chrisdbhr@outlook.com>
2020-09-13 17:22:40 -0300

    task extension name

--- SourceCode/src/Scripts/Extensions/TaskExtensions.cs
+++ SourceCode/src/Scripts/Extensions/TaskExtensions.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace NyuBot.Extensions {
 	public static class TaskExtensions {
-		public static async void AwaitAndForget(this Task task) {
+		public static async void CAwait(this Task task) {
 			await task;
 		}
 	}
