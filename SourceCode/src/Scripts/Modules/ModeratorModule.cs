38c20809aa89c2379482823ab27876c205fd689a
Chrisdbhr <chrisdbhr@outlook.com>
2020-10-01 03:08:15 -0300

    rename voice channel command

--- SourceCode/src/Scripts/Modules/ModeratorModule.cs
+++ SourceCode/src/Scripts/Modules/ModeratorModule.cs
@@ -15,8 +15,28 @@ namespace NyuBot.Modules
         [RequireBotPermission(GuildPermission.KickMembers)]
         public async Task Kick([Remainder]SocketGuildUser user)
         {
-            await ReplyAsync($"cya {user.Mention} :wave:");
+            await this.ReplyAsync($"cya {user.Mention} :wave:");
             await user.KickAsync();
+        }
+
+        [Command("rvc"), Alias("renamevoicechannel")]
+        [Summary("Renames a voice channel that user is in.")]
+        [RequireUserPermission(GuildPermission.ManageChannels)]
+        [RequireBotPermission(GuildPermission.ManageChannels)]
+        public async Task RenameVoiceChannel(string newVcName) {
+            if (string.IsNullOrEmpty(newVcName)) return;
+            if (!(this.Context.User is SocketGuildUser user)) return;
+            var vc = user.VoiceChannel;
+            if (vc == null) return;
+            var oldName = vc.Name;
+            await vc.ModifyAsync(p => p.Name = newVcName);
+            
+            var embed = new EmbedBuilder {
+                Title = "Canal de voz renomeado",
+                Description = $"**{oldName}** renomeado para **{newVcName}**"
+            };
+
+            await this.ReplyAsync("", false, embed.Build());
         }
     }
 }
