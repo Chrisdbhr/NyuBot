d279ba4f8471a9da0fb0743f9aad00e46a637876
Chrisdbhr <chrisdbhr@outlook.com>
2021-02-08 02:30:10 -0300

    get users account time

--- SourceCode/src/Scripts/Modules/UserModule.cs
+++ SourceCode/src/Scripts/Modules/UserModule.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -9,10 +9,9 @@ using NyuBot.Extensions;
 namespace NyuBot.Modules {
 	public class UserModule : ModuleBase<SocketCommandContext> {
 
-
 		[Command("profilepicture"), Alias("pp")]
 		[Summary("Get user profile picture")]
-		public async Task GetUserProfilePicture(SocketGuildUser user) {
+		public async Task GetUserProfilePicture(SocketGuildUser user = null) {
 			var userAvatarUrl = user.GetAvatarUrlSafe();
 			
 			var uri = new Uri(userAvatarUrl);
@@ -25,6 +24,34 @@ namespace NyuBot.Modules {
 			await this.ReplyAsync("", false, embed.Build());
 			await this.Context.Message.DeleteAsync();
 		}
-		
+
+		[Command("accounttime"), Alias("actime")]
+		[Summary("Get other user account creation time")]
+		public async Task GetUserAccountTime(SocketUser user = null) {
+			user ??= this.Context.User;
+			
+			var now = DateTime.UtcNow;
+			var created = user.CreatedAt.UtcDateTime;
+			var difference = (now - created);
+			
+			var totalMonths = difference.TotalDays / 30.4;
+			var years = totalMonths / 12;
+			var months = totalMonths % 12;
+			var days = difference.TotalDays / 30.4;
+			
+			var sb = new StringBuilder();
+			if (years > 0) sb.Append($"{years:0} ano(s), ");
+			if (months > 0) sb.Append($"{months:0} mes(es), ");
+			if (days > 0) sb.Append($"{days:0} dias");
+			
+			var e = new EmbedBuilder {
+				Title = sb.ToString(),
+				Description = $"Tempo de conta de {user.Mention}"
+			};
+			e.ImageUrl = user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl();
+
+			await this.ReplyAsync(this.Context.User.Mention, false, e.Build());
+		}
+
 	}
 }
