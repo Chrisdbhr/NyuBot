c0ea2f8452db1aaf2e0bcb46f4f7785b4d8ded8f
Chris <chrisdbhr@gmail.com>
2021-07-16 09:27:17 -0300

    auto react service wip

--- SourceCode/src/Scripts/Services/AutoReactService.cs
+++ SourceCode/src/Scripts/Services/AutoReactService.cs
@@ -1,5 +1,26 @@
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
 namespace NyuBot {
 	public class AutoReactService {
+
+		public AutoReactService(DiscordSocketClient discord) {
+			this._discord = discord;
+			this._discord.MessageReceived += this.OnMessageReceived;
+		}
+		
+
+		
+		
+		private readonly DiscordSocketClient _discord;
+
+		
+		
+		
+		private async Task OnMessageReceived(SocketMessage message) {
+			if (message.Attachments.Count <= 0) return;
+			
+		}
 		
 	}
 }
