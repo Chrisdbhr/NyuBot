d0720f5257ea8744c47f5a29c9ded6cb72d085d8
Chris <chrisdbhr@gmail.com>
2021-07-16 09:14:09 -0300

    string extensions

--- SourceCode/src/Scripts/Extensions/StringExtensions.cs
+++ SourceCode/src/Scripts/Extensions/StringExtensions.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Linq;
+using System.Text;
 
 namespace NyuBot.Extensions {
 	public static class StringExtensions {
+		
+		private static readonly StringBuilder sb = new StringBuilder();
+		
+		
+		
 		
 		public static string ReplaceAt(this string input, int index, char newChar)  {
 			if (input == null) {
@@ -11,14 +17,6 @@ namespace NyuBot.Extensions {
 			char[] chars = input.ToCharArray();
 			chars[index] = newChar;
 			return new string(chars);
-		}
-
-		public static string CSubstring(this string input, int startIndex, int length) {
-			if (string.IsNullOrEmpty(input)) return null;
-			if (length >= input.Length) {
-				return input;
-			}
-			return input.Substring(startIndex, length);
 		}
 		
 		public static string FirstCharToUpper(this string input) {
@@ -29,9 +27,49 @@ namespace NyuBot.Extensions {
 				default: return input.First().ToString().ToUpper() + input.Substring(1);
 			}
 		}
-
+		
 		public static string CJoin(this string[] input, char separator = ' ') {
 			return string.Join(separator, input);
+		}
+		
+		public static bool AreAllCharactersTheSame(this string str) {
+			if (string.IsNullOrEmpty(str)) return false;
+			if (str.Length == 1) return true;
+			var firstChar = str[0];
+			for (int i = 1; i < str.Length; i++) {
+				if (str[i] != firstChar) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		
+		/// <summary>
+		/// Remove special characters from string.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns>Return normalized string.</returns>
+		public static string RemoveDiacritics(this string text) {
+			var normalizedString = text.Normalize(NormalizationForm.FormD);
+			sb.Clear();
+
+			foreach (var c in normalizedString) {
+				var unicodeCategory = System.Globalization.CharUnicodeInfo.GetUnicodeCategory(c);
+				if (unicodeCategory != System.Globalization.UnicodeCategory.NonSpacingMark) {
+					sb.Append(c);
+				}
+			}
+
+			return sb.ToString().Normalize(NormalizationForm.FormC);
+		}
+		
+		public static string SubstringSafe(this string input, int length) {
+			if (string.IsNullOrEmpty(input)) return null;
+			if (length >= input.Length) {
+				return input;
+			}
+			return input.Substring(0, length);
 		}
 	}
 }
