64e1372e07ea44192235cbf24850c41d7ea2ac70
Chris <chrisdbhr@gmail.com>
2021-07-16 09:14:51 -0300

    version 1.5

--- SourceCode/src/Program.cs
+++ SourceCode/src/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace NyuBot {
     class Program {
+        public const string VERSION = "v1.5"; 
         public static Task Main(string[] args) => Startup.RunAsync(args);
     }
 }
