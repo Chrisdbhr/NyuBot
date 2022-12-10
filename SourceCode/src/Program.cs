f608f79e7f41f863f20f336efbff4e649412cc18
Chris <chrisdbhr@gmail.com>
2022-12-10 01:48:35 -0300

    updated to Discord.Net v3.8.1

--- SourceCode/src/Program.cs
+++ SourceCode/src/Program.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace NyuBot {
     class Program {
-        public const string VERSION = "v1.5"; 
+        public static readonly Version VERSION = new Version("5.0.0"); 
         public static Task Main(string[] args) => Startup.RunAsync(args);
     }
 }
